486f7238081f9e6cfb2b860616ed94d579238bb6
kouhei-matuoka <kouhei19951112@gmail.com>
2020-05-03 16:58:37 +0000

    Finish add-user-attendances

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
-module ApplicationHelper
+module AttendancesHelper
 
-  # ページごとにタイトルを返す
-  def full_title(page_name = "") # メソッドと引数の定義
-    base_title = "AttendanceApp" # 基本となるアプリケーション名を変数に代入
-    if page_name.empty? # 引数を受け取っているか判定
-      base_title # 引数page_nameが空文字の場合はbase_titleのみ返す
-    else # 引数page_nameが空文字ではない場合
-      page_name + " | " + base_title # 文字列を連結して返す
+  def attendance_state(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
+      return '出勤' if attendance.started_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    return false
+  end
+
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
   end
 end
